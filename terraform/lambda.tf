704e5a0323135b1c87ec3f30d6e0f4ab13173e4d
BharathWajKS <80683016+BharathWajKS@users.noreply.github.com>
2022-10-16 15:13:18 +0530

    Add files via upload

---
+++ terraform/lambda.tf
@@ -0,0 +1,23 @@
+resource "aws_lambda_function" "demo-lambda-function" {
+  depends_on = [
+    null_resource.ecr_image
+  ]
+  function_name = "${local.prefix}-lambda"
+  role          = aws_iam_role.lambda.arn
+  timeout       = 300
+  image_uri     = "${aws_ecr_repository.repo.repository_url}@${data.aws_ecr_image.lambda_image.id}"
+  package_type  = "Image"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.demo-lambda-function.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.books.execution_arn}/*/*"
+}
+
+output "lambda_name" {
+  value = aws_lambda_function.demo-lambda-function.id
+}
---
+++ terraform/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  prefix              = "demo"
+  app_dir             = "api"
+  account_id          = data.aws_caller_identity.current.account_id
+  ecr_repository_name = "${local.prefix}-demo-lambda-container"
+  ecr_image_tag       = "latest"
+}
+ 
